ca83ed2134951eaa4a4f75f46757b7df69df079b
treenhouse <deliazhou050416@gmail.com>
2024-08-17 16:44:48 +1000

    we're not running on next.js anymore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://gczejdgdwlfflnrgtpyg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjemVqZGdkd2xmZmxucmd0cHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3ODk3MjEsImV4cCI6MjAzOTM2NTcyMX0.SOIx5kheJ0Ekt-aTA5PD5cpG5gnV6JPV9L19XPsofwk");
 
 function App() {
   return (
