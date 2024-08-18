27f492f5e8666ba9363a91e04ffa60c54e7ff3d1
treenhouse <deliazhou050416@gmail.com>
2024-08-18 11:52:22 +1000

    deleted supabase files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from '../components/Header.js';
+import Header from './components/Header.js';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Header/>
         <main>{children}</main>
       </body>
     </html>
